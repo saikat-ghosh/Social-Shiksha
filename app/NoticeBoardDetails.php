5ea99b45e112b4a2539dfb11416c5a4a31a7eabc
Saikat Ghosh <saikat.ghosh.1994@outlook.com>
2017-08-04 03:37:27 +0530

    Notice Board model updated!

--- app/NoticeBoardDetails.php
+++ app/NoticeBoardDetails.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class NoticeBoardDetails extends Model
 {
-    //
+    protected $table = 'notice_board_details';
+
+    protected $fillable = ['NB_Heading','NB_Content','NB_T_Id','NB_Date','NB_Inst_Id','Role_Type','Ent_Type'];
 }
